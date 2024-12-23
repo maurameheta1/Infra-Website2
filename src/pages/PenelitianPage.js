b57cf776fbab9ec44dc45c2ba5bec309eb836478
maurameheta1 <mauramaheta05@gmail.com>
2024-12-23 15:06:18 +0700

    Aplikasi Frontend

---
+++ src/pages/PenelitianPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Crud from "../components/SipenaCrud";
+const PenelitianPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Halaman Data Penelitian</h1>
+      <Crud/>
+    </div>
+  );
+};
+
+export default PenelitianPage;
